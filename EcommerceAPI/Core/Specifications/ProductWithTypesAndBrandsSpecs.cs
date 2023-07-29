bd45b1fee9e4630d644345d3810b6fd81a3bb923
Aljamil1113 <aarazasaljamil@gmail.com>
2023-07-29 15:32:24 +0800

    API: Pagination

--- EcommerceAPI/Core/Specifications/ProductWithTypesAndBrandsSpecs.cs
+++ EcommerceAPI/Core/Specifications/ProductWithTypesAndBrandsSpecs.cs
@@ -10,16 +10,18 @@ namespace Core.Specifications
 {
     public class ProductWithTypesAndBrandsSpecs : BaseSpecification<Product>
     {
-        public ProductWithTypesAndBrandsSpecs(string sort, int? brandId, int? typeId)
-            : base(x => (!brandId.HasValue || x.ProductBrandId == brandId) && (!typeId.HasValue || x.ProductTypeId == typeId))
+        public ProductWithTypesAndBrandsSpecs(ProductSpecParams productSpecParams)
+            : base(x => (!productSpecParams.BrandId.HasValue || x.ProductBrandId == productSpecParams.BrandId) && 
+            (!productSpecParams.TypeId.HasValue || x.ProductTypeId == productSpecParams.TypeId))
         {
             AddInclude(x => x.ProductType);
             AddInclude(y => y.ProductBrand);
             AddOrderBy(x => x.Name);
+            ApplyPaging(productSpecParams.PageSize * (productSpecParams.PageIndex - 1), productSpecParams.PageSize);
 
-            if(!string.IsNullOrEmpty(sort))
+            if(!string.IsNullOrEmpty(productSpecParams.Sort))
             {
-                switch(sort)
+                switch(productSpecParams.Sort)
                 {
                     case "priceAsc":
                         AddOrderBy(p => p.Price);
